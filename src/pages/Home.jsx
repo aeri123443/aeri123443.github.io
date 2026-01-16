838079e0ea9432bac5050944eeb7e1565f6edab9
애리 <aeri123443@gmail.com>
2026-01-16 10:18:17 +0900

    [design] Home 페이지 마크업
    Home 페이지 마크업
    
    Resolves : #2

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import './Home.css'
+
+export default function Home() {
+  return (
+    <main class='main-home'>
+        <div class="header-main">
+            <div class="h2-main">
+                <h2>안녕하세요!</h2>
+                <h2><b>사용자 경험</b>과 <b>팀의 생산성</b>을 동시에 높이는</h2>
+                <h2><strong>웹 개발자</strong> <b>정애리</b>입니다.</h2>
+            </div>
+
+            <div class="h5-main">
+                <h5>
+                    <b>함께 끝까지 해내자</b>는 가치관으로, <b>구조와 흐름</b>을 고려하여, <br/>
+                    사용자 경험과 팀의 생산성을 동시에 높입니다.<br/>
+                    <br/>
+                    혼자 잘 쓰는 코드보다 <b>팀이 함께 쓰기 쉬운 코드</b>를 지향합니다.<br/>
+                    <b>프론트엔드와 백엔드의 경계를 넘나드는</b> 개발 경험에 관심이 있습니다.<br/>
+                    단기적인 성과보다 <b>장기적인 팀 생산성</b>을 함께 고려합니다.
+                </h5> 
+            </div>
+        </div>
+    </main>
+  );
+}
